b5409f108c50030ac3fb014b6ab61b9b0627b8a6
sriram <sri321ramhegde@gmail.com>
2018-10-13 18:39:21 +0530

    completed

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+var recipeName = "Creamy squash linguine";
+var preparationTime = 45;
+var recipeBy = "Jimmy Cutter";
+var isVegetarian = true;
+var calories = 223.7;
+
+console.log("Recipe details");
+console.log("-----------------------");
+console.log("Recipe Name: " + recipeName);
+console.log("Preparation Time: " + preparationTime);
+console.log("Recipe By: " + recipeBy);
+console.log("is Vegetarian: " + isVegetarian);
+console.log("Calories: " + calories);
+
+console.log('Recipe Details after update');
+console.log("---------------------------");
+preparationTime = 60;
+recipeBy = "Jimmy Cutter George";
+
+
+console.log("Recipe Name: " + recipeName);
+console.log("Preparation Time: " + preparationTime);
+console.log("Recipe By: " + recipeBy);
+console.log("is Vegetarian: " + isVegetarian);
+console.log("Calories: " + calories);
